a582460af86b8bd7e112716c0fbe2ca129854aa5
jodhy24 <jodhy.simanjuntak@indocyber.co.id>
2019-04-05 10:15:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/contentarticle/RegisterActivity.java
@@ -0,0 +1,44 @@
+package com.example.contentarticle;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextView login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        login = (TextView) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                // Destroy Aplikasi
+                    finish();
+                // bikin TOAST
+                Toast.makeText(RegisterActivity.this, "Silahkan Login Terlebih Dahulu", Toast.LENGTH_SHORT).show();
+
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+        finish();
+    }
+}
